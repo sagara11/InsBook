7e44b58fb0af4e80d4517755af95d4b1f55210a3
Tran Hung Phong <cityzen0910@gmail.com>
2020-03-02 22:43:01 +0700

    update trang chu, hieu ung tha tim, phan them bai dang

--- InsBook/Assets/js/love_button.js
+++ InsBook/Assets/js/love_button.js
@@ -2,8 +2,10 @@
     if ($("#" + id).hasClass("liked")) {
         $("#" + id).html('<i class="fa fa-heart-o" aria-hidden="true"></i>');
         $("#" + id).removeClass("liked");
+        $("#" + id + "-modal").html('<i class="fa fa-heart-o" aria-hidden="true"></i>');
     } else {
         $("#" + id).html('<i class="fa fa-heart" aria-hidden="true"></i>');
         $("#" + id).addClass("liked");
+        $("#" + id + "-modal").html('<i class="fa fa-heart" aria-hidden="true"></i>');
     }
 }
