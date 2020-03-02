594ccc1a3d0fe0242e19c9db5e353109764b5292
KAD1202 <nhabachocdung@gmail.com>
2020-03-03 00:06:18 +0700

    Sửa xong js

--- InsBook/Assets/javascript/Register.js
+++ InsBook/Assets/javascript/Register.js
@@ -7,9 +7,10 @@ $("#submit").on("click", function (e) {
     var i = 0;
     ids.forEach(function (id) {
         value[i] = $(id).val();
+        console.log(value[i]);
         if (value[i]) {
             count += 1;
-            $("#check-"+ id).attr("class", "success");
+            $("#check-" + id).attr("class", "success");
             $("#check-" + id).html("<i class='fas fa-check-circle' style='opacity:1;visibility: visible'></i>");
         }
         else {
@@ -19,8 +20,7 @@ $("#submit").on("click", function (e) {
         i++;
     });
     if (count == ids.length) {
-        console.log("hahahah");
-        return false;
+        return true;
     }
     return false;
 });
@@ -36,14 +36,22 @@ $("#Email").blur(function () {
             email: email
         },
         success: function (result) {
-            if (result.status) {
-                $("#check-Email").attr("class", "errors");
-                $("#check-Email").html("<i class='fas fa-times' style='opacity:1;visibility: visible'></i>");
+            var result = $.parseJSON(result);
+            var check = isEmail(email);
+            if (check) {
+                if (result.status) {
+                    $("#check-Email").attr("class", "errors");
+                    $("#check-Email").html("<i class='fas fa-times' style='opacity:1;visibility: visible'></i>");
+                }
+                else {
+                    $("#check-Email").attr("class", "success");
+                    $("#check-Email").html("<i class='fas fa-check-circle' style='opacity:1;visibility: visible'></i>");
+                }
             }
             else {
-                $("#check-Email").attr("class", "success");
-                $("#check-Email").html("<i class='fas fa-check-circle' style='opacity:1;visibility: visible'></i>");
-            }
+                $("#check-Email").attr("class", "errors");
+                $("#check-Email").html("<i class='fas fa-times' style='opacity:1;visibility: visible'></i>");
+            }  
         }
     });
 });
@@ -63,4 +71,70 @@ $("#XacnhanMatkhau").blur(function () {
     }
 });
 
+// hàm check email
+function isEmail(emailStr) {
+    var emailPat = /^(.+)@(.+)$/;
+    var specialChars = "\\(\\)<>@,;:\\\\\\\"\\.\\[\\]";
+    var validChars = "\[^\\s" + specialChars + "\]";
+    var quotedUser = "(\"[^\"]*\")";
+    var ipDomainPat = /^\[(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})\]$/;
+    var atom = validChars + '+';
+    var word = "(" + atom + "|" + quotedUser + ")";
+    var userPat = new RegExp("^" + word + "(\\." + word + ")*$");
+    var domainPat = new RegExp("^" + atom + "(\\." + atom + ")*$");
+    var matchArray = emailStr.match(emailPat);
 
+    if (matchArray == null) {
+        return false;
+    }
+
+    var user = matchArray[1];
+    var domain = matchArray[2];
+
+    if (user.match(userPat) == null) {
+        return false;
+    }
+
+    var IPArray = domain.match(ipDomainPat);
+
+    if (IPArray != null) {
+        for (var i = 1; i <= 4; i++) {
+            if (IPArray[i] > 255) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    var domainArray = domain.match(domainPat);
+
+    if (domainArray == null) {
+        return false;
+    }
+
+    var atomPat = new RegExp(atom, "g");
+    var domArr = domain.match(atomPat);
+    var len = domArr.length;
+
+    if (domArr[domArr.length - 1].length < 2 || domArr[domArr.length - 1].length > 3) {
+        return false;
+    }
+
+    if (len < 2) {
+        return false;
+    }
+    return true;
+}
+function is_email($str) {
+    return (!preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $str)) ? false : true;
+}
+// hàm check số điện thoại
+function isPhone($phoneStr) {
+    var vnf_regex = /((09|03|07|08|05)+([0-9]{8})\b)/g;
+
+    if (vnf_regex.test($phoneStr) == false) {
+        return false;
+    } else {
+        return true
+    }
+}
