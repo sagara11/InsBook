4e28a35b7c62c25efcf2326dd8baefecc1e269be
Minh Thong <thiensangkdtt@gmail.com>
2020-03-04 18:13:24 +0700

    Lỗi js

--- InsBook/Assets/javascript/Register.js
+++ InsBook/Assets/javascript/Register.js
@@ -1,44 +1,18 @@
-﻿//kiểm tra sau khi bấm đăng kí
-$("#submit").on("click", function (e) {
-    //var ho = $().;
-    var ids = ["#Ho", "#Ten", "#Email", "#Matkhau", "#XacnhanMatkhau", "#Ngaysinh", "#Gioitinh"];
-    var count = 0;
-    var value = [];
-    var i = 0;
-    ids.forEach(function (id) {
-        value[i] = $(id).val();
-        console.log(value[i]);
-        if (value[i]) {
-            count += 1;
-            $("#check-" + id).attr("class", "success");
-            $("#check-" + id).html("<i class='fas fa-check-circle' style='opacity:1;visibility: visible'></i>");
-        }
-        else {
-            $("#check-" + id).attr("class", "errors");
-            $("#check-" + id).html("<i class='fas fa-times' style='opacity:1;visibility: visible'></i>");
-        }
-        i++;
-    });
-    if (count == ids.length) {
-        return true;
-    }
-    return false;
-});
-
-// email đăng ký đã tồn tại chưa
-$("#Email").blur(function () {
-    var email = $("#Email").val();
-    $.ajax({
-        url: "/Client/Register/CheckEmail",
-        type: "get",
-        dataType: "text",
-        data: {
-            email: email
-        },
-        success: function (result) {
-            var result = $.parseJSON(result);
-            var check = isEmail(email);
-            if (check) {
+﻿// tạo đối tượng check
+var check = {
+    email: function () { // hàm checkemail
+        var email = $("#Email").val();
+        $.ajax({
+            url: "/Client/Register/CheckEmail",
+            type: "get",
+            dataType: "text",
+            data: {
+                email: email
+            }
+        }).done(function (results) {
+            var result = $.parseJSON(results);
+            var checkemail = isEmail(email);
+            if (checkemail) {
                 if (result.status) {
                     $("#check-Email").attr("class", "errors");
                     $("#check-Email").html("<i class='fas fa-times' style='opacity:1;visibility: visible'></i>");
@@ -51,24 +25,87 @@ $("#Email").blur(function () {
             else {
                 $("#check-Email").attr("class", "errors");
                 $("#check-Email").html("<i class='fas fa-times' style='opacity:1;visibility: visible'></i>");
-            }  
+            }
+        });
+    },
+    pass: function () { // hàm check mật khẩu
+        var pass = $("#Matkhau").val();
+        var confirm = $("#XacnhanMatkhau").val();
+        if (pass == confirm) {
+            // $('.blue').removeClass('blue').addClass('green');
+            // $("#td_id").toggleClass('change_me newClass');
+            $("#check-XacnhanMatkhau").attr("class", "success");
+            $("#check-XacnhanMatkhau").html("<i class='fas fa-check-circle' style='opacity:1;visibility: visible'></i>");
+            return true;
+        } else {
+            $("#check-XacnhanMatkhau").attr("class", "errors");
+            $("#check-XacnhanMatkhau").html("<i class='fas fa-times' style='opacity:1;visibility: visible'></i>");
+            return false;
         }
+    }
+};
+
+//kiểm tra sau khi bấm đăng kí
+$("#submit").on("click", function () {
+    var ids = ["checkbox", "Gioitinh", "Ngaysinh", "XacnhanMatkhau", "Matkhau", "Email", "Ten", "Ho"];
+    var count = 0;
+    var value = [];
+    var i = 0;
+    ids.forEach(function (id) {
+        value[i] = $("#" + id).val();
+        console.log(value[i]);
+        if (value[i]) {
+            if (id === "Email") {
+                var email = $("#" +  id).val();
+                $.ajax({
+                    url: "/Client/Register/CheckEmail",
+                    type: "get",
+                    dataType: "text",
+                    data: {
+                        email: email
+                    }
+                }).done(function (results) {
+                    var result = $.parseJSON(results);
+                    var checkemail = isEmail(email);
+                    if (checkemail) {
+                        if (result.status == false) {
+                            count += 1;
+                        }
+                    }
+                });
+            }
+            else if (id === "XacnhanMatkhau") {
+                if (check.pass()) {
+                    count += 1;
+                }
+            }
+            else {
+                count += 1;
+                $("#check-" + id).attr("class", "success");
+                $("#check-" + id).html("<i class='fas fa-check-circle' style='opacity:1;visibility: visible'></i>");
+            }
+        }
+        else {
+            $("#check-" + id).attr("class", "errors");
+            $("#check-" + id).html("<i class='fas fa-times' style='opacity:1;visibility: visible'></i>");
+            $("#" + id).focus();
+        }
+        i++;
     });
+    if (count == ids.length) {
+        return true;
+    }
+    return false;
+});
+
+// email đăng ký đã tồn tại chưa
+$("#Email").blur(function () {
+    check.email();
 });
 
 // kiểm tra xác nhận mật khẩu
 $("#XacnhanMatkhau").blur(function () {
-    var pass = $("#Matkhau").val();
-    var confirm = $("#XacnhanMatkhau").val();
-    if (pass == confirm) {
-        // $('.blue').removeClass('blue').addClass('green');
-        // $("#td_id").toggleClass('change_me newClass');
-        $("#check-XacnhanMatkhau").attr("class", "success");
-        $("#check-XacnhanMatkhau").html("<i class='fas fa-check-circle' style='opacity:1;visibility: visible'></i>");
-    } else {
-        $("#check-XacnhanMatkhau").attr("class", "errors");
-        $("#check-XacnhanMatkhau").html("<i class='fas fa-times' style='opacity:1;visibility: visible'></i>");
-    }
+    check.pass();
 });
 
 // hàm check email
