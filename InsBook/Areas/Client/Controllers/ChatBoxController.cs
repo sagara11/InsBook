f656be62ac983ef1168a6afc810cc0f8859c04dd
Minh Thong <thiensangkdtt@gmail.com>
2020-03-05 21:59:24 +0700

    them phan chatbox

---
+++ InsBook/Areas/Client/Controllers/ChatBoxController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InsBook.Areas.Client.Controllers
+{
+    public class ChatBoxController : Controller
+    {
+        // GET: Client/ChatBox
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
