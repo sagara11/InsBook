e488ff2b75fbf946804578ec9f91f638412227c6
ha ha Kkingg <kkingg1209@gmail.com>
2020-03-16 20:06:42 +0700

    update trang ca nhan

--- InsBook/Areas/Client/Controllers/PersonalController.cs
+++ InsBook/Areas/Client/Controllers/PersonalController.cs
@@ -13,5 +13,49 @@ namespace InsBook.Areas.Client.Controllers
         {
             return View();
         }
+        public ActionResult ChangeGeneralInfor()
+        {
+            return View();
+        }
+        public ActionResult ChangePassInfo()
+        {
+            return View();
+        }
+        public ActionResult Job_Edu()
+        {
+            return View();
+        }
+        public ActionResult LivingPlace()
+        {
+            return View();
+        }
+        public ActionResult Rela_Fami()
+        {
+            return View();
+        }
+        public ActionResult DetailInfo()
+        {
+            return View();
+        }
+        public ActionResult FriendsInfo()
+        {
+            return View();
+        }
+        public ActionResult ImagesInfo()
+        {
+            return View();
+        }
+        public ActionResult SavedInfo()
+        {
+            return View();
+        }
+        public ActionResult AlbumInfo()
+        {
+            return View();
+        }
+        public ActionResult VideoInfo()
+        {
+            return View();
+        }
     }
 }
--- InsBook/Assets/js/user-info.js
+++ InsBook/Assets/js/user-info.js
@@ -1,4 +1,112 @@
 function auto_grow(element) {
     element.style.height = "5px";
-    element.style.height = (element.scrollHeight)+"px";
+    element.style.height = (element.scrollHeight) + "px";
 }
+$("#show-job-adding-box").click(function () {
+    $(".job-adding-box").css("display", "block");
+    $(".job-adding").css("display", "none");
+});
+$("#close-job-adding-box").click(function () {
+    $(".job-adding-box").css("display", "none");
+    $(".job-adding").css("display", "block");
+});
+$(".job-closse").click(function () {
+    $(".job-adding-box").css("display", "none");
+    $(".job-adding").css("display", "block");
+});
+$("#show-university-adding-box").click(function () {
+    $(".university-adding-box").css("display", "block");
+    $(".university-adding").css("display", "none");
+});
+$("#close-university-adding-box").click(function () {
+    $(".university-adding-box").css("display", "none");
+    $(".university-adding").css("display", "block");
+});
+$(".university-closse").click(function () {
+    $(".university-adding-box").css("display", "none");
+    $(".university-adding").css("display", "block");
+});
+$("#show-highschool-adding-box").click(function () {
+    $(".highschool-adding-box").css("display", "block");
+    $(".highschool-adding").css("display", "none");
+});
+$("#close-highschool-adding-box").click(function () {
+    $(".highschool-adding-box").css("display", "none");
+    $(".highschool-adding").css("display", "block");
+});
+$(".highschool-closse").click(function () {
+    $(".highschool-adding-box").css("display", "none");
+    $(".highschool-adding").css("display", "block");
+});
+$("#show-country-lived-adding-box").click(function () {
+    $(".country-lived-adding-box").css("display", "block");
+    $(".country-lived-adding").css("display", "none");
+});
+$("#close-country-lived-adding-box").click(function () {
+    $(".country-lived-adding-box").css("display", "none");
+    $(".country-lived-adding").css("display", "block");
+});
+$(".country-lived-closse").click(function () {
+    $(".country-lived-adding-box").css("display", "none");
+    $(".country-lived-adding").css("display", "block");
+});
+$("#show-relationship-adding-box").click(function () {
+    $(".relationship-adding-box").css("display", "block");
+    $(".relationship-adding").css("display", "none");
+});
+$("#close-relationship-adding-box").click(function () {
+    $(".relationship-adding-box").css("display", "none");
+    $(".relationship-adding").css("display", "block");
+});
+$(".relationship-closse").click(function () {
+    $(".relationship-adding-box").css("display", "none");
+    $(".relationship-adding").css("display", "block");
+});
+$("#show-family-adding-box").click(function () {
+    $(".family-adding-box").css("display", "block");
+    $(".family-adding").css("display", "none");
+});
+$("#close-family-adding-box").click(function () {
+    $(".family-adding-box").css("display", "none");
+    $(".family-adding").css("display", "block");
+});
+$(".family-closse").click(function () {
+    $(".family-adding-box").css("display", "none");
+    $(".family-adding").css("display", "block");
+});
+$("#show-description-adding-box").click(function () {
+    $(".description-adding-box").css("display", "block");
+    $(".description-adding").css("display", "none");
+});
+$("#close-description-adding-box").click(function () {
+    $(".description-adding-box").css("display", "none");
+    $(".description-adding").css("display", "block");
+});
+$(".description-closse").click(function () {
+    $(".description-adding-box").css("display", "none");
+    $(".description-adding").css("display", "block");
+});
+$("#show-quote-adding-box").click(function () {
+    $(".quote-adding-box").css("display", "block");
+    $(".quote-adding").css("display", "none");
+});
+$("#close-quote-adding-box").click(function () {
+    $(".quote-adding-box").css("display", "none");
+    $(".quote-adding").css("display", "block");
+});
+$(".quote-closse").click(function () {
+    $(".quote-adding-box").css("display", "none");
+    $(".quote-adding").css("display", "block");
+});
+$("#show-nickname-adding-box").click(function () {
+    $(".nickname-adding-box").css("display", "block");
+    $(".nickname-adding").css("display", "none");
+});
+$("#close-nickname-adding-box").click(function () {
+    $(".nickname-adding-box").css("display", "none");
+    $(".nickname-adding").css("display", "block");
+});
+$(".nickname-closse").click(function () {
+    $(".nickname-adding-box").css("display", "none");
+    $(".nickname-adding").css("display", "block");
+});
