7c0d09ab5635c0e4977b0ed00f413f200c6de2eb
Minh Thong <thiensangkdtt@gmail.com>
2020-06-01 16:56:43 +0700

    css

--- InsBook/Areas/Client/Controllers/HomeController.cs
+++ InsBook/Areas/Client/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ using System.Web.Mvc;
 
 namespace InsBook.Areas.Client.Controllers
 {
-    public class HomeController: BaseController // !!! mai sau sửa lại thành Base !!!
+    public class HomeController : BaseController // !!! mai sau sửa lại thành Base !!!
     {
         // GET: Client/Home
         public ActionResult Index()
@@ -30,6 +30,14 @@ namespace InsBook.Areas.Client.Controllers
             var tendd = new LocationDao().GetAllName();
             ViewBag.TenDD = tendd;
             ViewBag.UserOnline = CommonConstants.USER_ONLINE;
+            if (!CommonConstants.USER_ONLINE.Contains(user.UserID))
+            {
+                ViewBag.CheckOnline = "$(function () {$.connection.hub.start().done(function () {checkLogin(" + user.UserID + ")});});";
+            }
+            else
+            {
+                ViewBag.CheckOnline = " ";
+            }
             //var tenchude = new TopicDao().GetAllName();
             //ViewBag.TenDD = tendd;
             // hàm này để load tất cả bài viết trong trang cá nhân
--- InsBook/Assets/javascript/Login.js
+++ InsBook/Assets/javascript/Login.js
@@ -1,20 +1,21 @@
 ﻿$(function () {
     // Reference the auto-generated proxy for the hub.
     var session = $.connection.sessionHub;
+
     session.client.Online = function (userID) {
         $("#check-online-" + userID).html("Offline");
     };
     session.client.Login = function (userID) {
         $("#check-online-" + userID).html("Đang online");
     };
-
+    
     $.connection.hub.start().done(function () {
         checkOnlineRT = function (data) {
             session.server.checkOnline(data);
         }
 
         checkLoginRT = function (data) {
-            session.server.checkLogin(data)
+            session.server.checkLogin(data);
         }
     });
 });
@@ -23,17 +24,14 @@ function checkOnlineRT(data) {
 
 }
 function checkOnline(data) {
-    checkOnlineRT(data)
+    checkOnlineRT(data);
 }
 
 function checkLoginRT(data) {
 
 }
-$(window).load(function () {
-    var id = $(".user-name-login").attr('id');
 
-    id = id.split("-");
-    id = post_id[post_id.length - 1];
-
+function checkLogin(data) {
+    var id = data;
     checkLoginRT(id);
-});
+}
